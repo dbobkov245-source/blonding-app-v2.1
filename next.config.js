d6bb411c6202304879de955b3480e2287400e25c
dbobkov245-source <dbobkov245@gmail.com>
2025-11-08 22:46:18 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,17 +1,13 @@
 /** @type {import('next').NextConfig} */
-
-// 1. Импортируем 'next-pwa'
 const withPWA = require('next-pwa')({
-  dest: 'public', // Куда складывать service-worker
-  register: true, // Автоматически регистрировать
-  skipWaiting: true, // Не ждать, пока пользователь закроет вкладку
-  disable: process.env.NODE_ENV === 'development' // Отключаем в режиме разработки
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development'
 });
 
-// 2. Оборачиваем наш конфиг в 'withPWA'
 const nextConfig = withPWA({
   reactStrictMode: true,
-  // (здесь могут быть другие твои настройки, если они появятся)
 });
 
 module.exports = nextConfig;
