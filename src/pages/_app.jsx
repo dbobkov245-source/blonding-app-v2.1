72839d8213d58d77e707095f020a3cafc5b10e4d
dbobkov245-source <dbobkov245@gmail.com>
2025-11-08 14:06:41 +0300

    Update _app.jsx

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -21,7 +21,25 @@ export default function MyApp({ Component, pageProps }) {
           </Link>
         </div>
       </nav>
+<nav className='bg-white shadow-sm'>
+  <div className='max-w-4xl mx-auto p-4 flex gap-4 items-center'>
+    <Link href='/' className='font-semibold text-lg'>
+      Blonding App v2.1
+    </Link>
+    <Link href='/Theory' className='text-sm hover:underline'>
+      Теория
+    </Link>
+    <Link href='/Chat' className='text-sm hover:underline'>
+      AI-консультант
+    </Link>
 
+    {/* ВОТ ЭТУ СТРОКУ МЫ ДОБАВИЛИ */}
+    <Link href='/Test' className='text-sm hover:underline'>
+      Тест
+    </Link>
+
+  </div>
+</nav>
       {/* 3. Здесь будет отображаться текущая страница (Home, Theory и т.д.) */}
       <main className='max-w-4xl mx-auto p-6'>
         <Component {...pageProps} />
